a7399df9c65ad8b8d8a8eb0917534614206fac62
haruna1256 <haru_kawagishi@icloud.com>
2025-07-22 15:43:21 +0900

    Initial Commit

---
+++ ThankYouDays/ThankYouDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThankYouDaysApp.swift
+//  ThankYouDays
+//
+//  Created by 川岸遥奈 on 2025/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ThankYouDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
