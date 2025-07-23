fead6a57290f3fbb0713e11eafe8e358656e9452
haruna1256 <haru_kawagishi@icloud.com>
2025-07-23 10:32:16 +0900

    [add] 色合い定義

---
+++ ThankYouDays/Color.swift
@@ -0,0 +1,29 @@
+//
+//  Color.swift
+//  ThankYouDays
+//
+//  Created by 川岸遥奈 on 2025/07/23.
+//
+
+import SwiftUI
+
+// 色の定義
+extension Color {
+    // 背景
+    static let backgroundIvory = Color(red: 1.0, green: 0.976, blue: 0.957)   // #FFF974
+    
+    // 箱（Box）グラデーション用カラー
+    static let boxOrangeStart = Color(red: 1.0, green: 0.435, blue: 0.314) // #FF6F50
+    static let boxOrangeEnd = Color(red: 1.0, green: 0.604, blue: 0.439) // #FF9A70
+    
+    // ありがとう風船
+    static let thanksPink = Color(red: 1.0, green: 0.714, blue: 0.757) // #FFB6C1
+    static let thanksBlue = Color(red: 0.529, green: 0.808, blue: 0.980) // #87CEFA
+    static let thanksMint = Color(red: 0.690, green: 0.878, blue: 0.902) // #B0E0E6
+    
+    // テキスト
+    static let textDarkGray = Color(red: 0.2, green: 0.2, blue: 0.2) // #333333
+    
+    // ボタンの影など
+    static let softOrange = Color(red: 1.0, green: 0.604, blue: 0.439) // #FF9A70
+}
